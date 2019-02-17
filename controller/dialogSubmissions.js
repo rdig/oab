e69b14051acb42d9e765b937cc47848a5fa6a2e8
Raul Glogovetan <raul@glogovetan.com>
2019-02-17 19:03:51 +0200

    Fix: acconledge the slash command reply first

--- controller/dialogSubmissions.js
+++ controller/dialogSubmissions.js
@@ -21,13 +21,11 @@ module.exports = async controller => {
         `Submission Successful! You can check all the other submissions here: https://docs.google.com/spreadsheets/d/${process.env.spreadSheetId}`
       );
       try {
+        saveDialogToSheets(oab, event, controller);
         /*
          * @NOTE Call dialogOk or else Slack will think this is an error
-         * We need to call this prior to saving the actual data, since otherwise
-         * Slack will declare a timeout while waiting for the google api to finish
          */
         oab.dialogOk();
-        saveDialogToSheets(oab, event, controller);
       } catch (error) {
         oab.dialogError('Could not post your submission to the Spreadsheet');
       }
--- skills/slashCommandAccountable.js
+++ skills/slashCommandAccountable.js
@@ -2,6 +2,11 @@ const accountabilityDialog = require('../components/accountabilityDialog');
 
 module.exports = controller => {
   controller.on('slash_command', (oab, event) => {
+    /*
+     * @NOTE Acknoledge the slash command in the 3000 ms time frame first, to
+     * prevent the Timeout Slack error
+     */
+    oab.replyAcknowledge();
     oab.replyWithDialog(
       event,
       accountabilityDialog(oab).asObject(),
