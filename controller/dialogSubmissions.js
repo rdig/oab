fb3735ef4460c22b7b54c64d1d7d4be10cbd556b
Raul Glogovetan <raul@glogovetan.com>
2019-03-10 00:40:30 +0200

    Remove: interactive menu after rating submission

--- controller/dialogSubmissions.js
+++ controller/dialogSubmissions.js
@@ -1,5 +1,6 @@
+require('node-env-file')('.env');
+const util = require('util');
 const saveDialogToSheets = require('../lib/saveDialogToSheets');
-const interactiveMenuRatePublic = require('../components/interactiveMenuRatePublic');
 
 module.exports = async controller => {
   controller.middleware.receive.use((oab, event, next) => {
@@ -11,8 +12,9 @@ module.exports = async controller => {
     next();
   });
 
-  // handle a dialog submission
-  // the values from the form are in event.submission
+  /*
+   * Handle a dialog submission
+   */
   controller.on(
     'dialog_submission',
     async (oab, event) => {
@@ -20,12 +22,28 @@ module.exports = async controller => {
        * @NOTE Call dialogOk or else Slack will think this is an error
        */
       oab.dialogOk();
+      /*
+       * Get the user that was just rated,
+       * so we can use it in the confirmation message
+       *
+       * @TODO Reduce code repetition
+       * This is used both here and inside `saveDialogToSheets`.
+       * Maybe find a way to call this method just once.
+       */
+      const { user: { real_name: accountableUser } } =
+        await util.promisify(oab.api.users.info)({
+          user: event.submission.accountableUser,
+        });
+      oab.whisper(
+        event,
+        `Your rating was submitted successfully! *${accountableUser}* was aslo notified!
+_You can see all the other submissions in <https://docs.google.com/spreadsheets/d/${process.env.spreadSheetId}|this spreadsheet>_`
+      );
       try {
         saveDialogToSheets(oab, event, controller);
       } catch (error) {
         oab.dialogError('Could not post your submission to the Spreadsheet');
       }
-      interactiveMenuRatePublic(oab, event);
     },
   );
 };
