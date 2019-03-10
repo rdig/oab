989ea763e073641a5f48f95db958f24d6e07a66d
Raul Glogovetan <raul@glogovetan.com>
2019-03-10 05:00:37 +0200

    Add: `interactiveMenu` handle rating reponse

--- controller/interactiveMenu.js
+++ controller/interactiveMenu.js
@@ -1,6 +1,7 @@
 require('node-env-file')('.env');
 const updateSheetsValues = require('../lib/updateSheetsValues');
 const getUserInfo = require('../utils/getUserInfo');
+const responseDialog = require('../components/responseDialog');
 
 module.exports = controller => controller.on(
   'interactive_message_callback',
@@ -10,10 +11,45 @@ module.exports = controller => controller.on(
      * prevent the Timeout Slack error
      */
     oab.replyAcknowledge();
-
+    const user = await getUserInfo(oab, event.user);
     switch (event.text) {
+      case 'respond': {
+        const { displayName, userId } = user;
+        /*
+         * Spawn the reponse dialog
+         */
+        return oab.replyWithDialog(
+          event,
+          responseDialog(oab).asObject(),
+          (replyError, response) => {
+            if (replyError) {
+              return oab.botkit.log(
+                `Could not create the response dialog user ${displayName} (${userId})`,
+                replyError,
+              );
+            }
+            /*
+             * Delete the last message (via the API directly)
+             */
+            bot.api.im.history(
+              { channel: event.channel, count: 1 },
+              (apiError, historyResponse) => {
+                if (apiError) {
+                  return oab.botkit.log(
+                    `Could not get the last message for channel ${event.channel}`,
+                    replyError,
+                  );
+                }
+                return bot.api.chat.delete({
+                  channel: event.channel,
+                  ts: historyResponse.messages[0].ts,
+                });
+              },
+            );
+          },
+        );
+      }
       case 'acknowledge': {
-        const user = await getUserInfo(oab, event.user);
         updateSheetsValues(oab, controller, user, 'Acknowledged');
         return oab.replyInteractive(event, {
           text: "Ok! I've sent an acknowledgement on your behalf."
