0f36c64e9e5922d29a0e7382641f8dcf192dc2e0
Raul Glogovetan <raul@glogovetan.com>
2019-03-17 22:00:39 +0200

    Refactor: rating dialog now gets it's own user list
    (and displays it inside the select field)

--- controller/slashCommand.js
+++ controller/slashCommand.js
@@ -1,9 +1,10 @@
+const util = require('util');
 const accountabilityDialog = require('../components/accountabilityDialog');
 const showHelp = require('../skills/showHelp');
 const showStats = require('../skills/showStats');
 
 module.exports = controller => {
-  controller.on('slash_command', (oab, event) => {
+  controller.on('slash_command', async (oab, event) => {
     /*
      * @NOTE Acknoledge the slash command in the 3000 ms time frame first, to
      * prevent the Timeout Slack error
@@ -28,9 +29,10 @@ module.exports = controller => {
        * @NOTE For everything else, we open the rating dialog
        */
       default: {
+        const ratingDialog = await accountabilityDialog(oab, event);
         return oab.replyWithDialog(
           event,
-          accountabilityDialog(oab).asObject(),
+          ratingDialog.asObject(),
         );
       };
     };
